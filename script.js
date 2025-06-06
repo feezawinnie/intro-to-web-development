8ccdd6452af4c49fba1ea4e1bfa442a9426ca384
feeza <feezawinnie5@gmail.com>
2025-06-06 00:40:11 +0000

    resolved

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function changeBackgroundColor(color){
    let section = document.getElementById('coding-journey');
    section.style.backgroundColor = color; 
@@ -15,21 +15,3 @@ document.getElementById('backend').addEventListener('click', () => changeBackgro
 const currentYear = new Date().getFullYear();
 
 document.getElementById('year').textContent = currentYear;
-=======
-function changeBackgroundColor(color) {
-  let section = document.getElementById("coding-journey");
-  section.style.backgroundColor = color;
-}
-
-document
-  .getElementById("intro")
-  .addEventListener("click", () => changeBackgroundColor("#2CA02C"));
-
-document
-  .getElementById("react")
-  .addEventListener("click", () => changeBackgroundColor("#FF7F0E"));
-
-document
-  .getElementById("backend")
-  .addEventListener("click", () => changeBackgroundColor("#9467BD"));
->>>>>>> 9983733d782580a8df3697b513fbb52a20232ce4
